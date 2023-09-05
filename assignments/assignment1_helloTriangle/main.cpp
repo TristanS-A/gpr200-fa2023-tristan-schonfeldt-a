bf65cdd669a212bb6c959ef0e76bdbd9ee2da83f
Tristan Schonfeldt-Aultman <tristan.schonfeldt-a@mymail.champlain.edu>
2023-09-05 11:18:06 -0400

    Forgot to save ;-;

--- assignments/assignment1_helloTriangle/main.cpp
+++ assignments/assignment1_helloTriangle/main.cpp
@@ -8,6 +8,20 @@
 const int SCREEN_WIDTH = 1080;
 const int SCREEN_HEIGHT = 720;
 
+float verticies[9] = {
+		0.0, 0.5, 0.0, //Top
+		0.5, -0.5, 0.0,  //Right
+		-0.5, -0.5, 0.0  //Left
+};
+
+const char* vertexShaderSource = R"(
+	#version 450
+	layout(location = 0) in vec3 vPos;
+	void main(){
+	gl_Position = vec4(vPos,1.0);
+	}
+)";
+
 int main() {
 	printf("Initializing...");
 	if (!glfwInit()) {
@@ -26,6 +40,18 @@ int main() {
 		printf("GLAD Failed to load GL headers");
 		return 1;
 	}
+
+	unsigned int vao;
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+	glBindBuffer(GL_ARRAY_BUFFER, vao);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, (const void*)0);
+	glEnableVertexAttribArray(0);
+
+	unsigned int vbo;
+	glGenBuffers(1, &vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(verticies), verticies, GL_STATIC_DRAW);
 
 	while (!glfwWindowShouldClose(window)) {
 		glfwPollEvents();
