c54f1547b3fc52020c2e0bc69c1b7776566d973a
TristanS-A <tristan.schonfeldt-a@mymail.champlain.edu>
2023-09-06 14:40:49 -0400

    Finished up the project by adding comments, tydying up functions, and made the background change color.

--- assignments/assignment1_helloTriangle/main.cpp
+++ assignments/assignment1_helloTriangle/main.cpp
@@ -36,33 +36,45 @@ const char* fragmentShaderSource = R"(
     }
 )";
 
-unsigned int createVAO(float* vertexData, int numVertices){
+unsigned int createVAO(float* vertexData, int numVertices, int length){
+
+    //Creates vertex buffer object
     unsigned int vbo;
     glGenBuffers(1, &vbo);
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(verticies), vertexData, GL_STATIC_DRAW);
+    //Assigns vertices to vertex buffer
+    glBufferData(GL_ARRAY_BUFFER, static_cast<GLsizeiptr>(sizeof(float[length])), vertexData, GL_STATIC_DRAW);
 
+    //Creates vertex array object
     unsigned int vao;
     glGenVertexArrays(1, &vao);
     glBindVertexArray(vao);
+    //Assigns vertex buffer object tp vao
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
 
-    const int NUM_COLORS = 4;
-
-    glVertexAttribPointer(0, numVertices, GL_FLOAT, GL_FALSE, static_cast<int>(sizeof(float)) * (numVertices + NUM_COLORS), (const void*)0);
+    //Creates attribute pointer to position
+    glVertexAttribPointer(0, numVertices, GL_FLOAT, GL_FALSE, static_cast<int>(sizeof(float)) * (length / numVertices), (const void*)0);
+    //Enables attribute at location 0;
     glEnableVertexAttribArray(0);
 
-    glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, static_cast<int>(sizeof(float)) * (numVertices + NUM_COLORS), (const void*)(sizeof(float)*3));
+    //Creates attribute pointer to color
+    glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, static_cast<int>(sizeof(float)) * (length / numVertices), (const void*)(sizeof(float)*3));
+    //Enables attribute at location 1
     glEnableVertexAttribArray(1);
 
     return vao;
 }
 
 unsigned int createShader(GLenum shaderType, const char* sourceCode){
+
+    //Creates shader
     unsigned int shader = glCreateShader(shaderType);
+    //Adds shader source code
     glShaderSource(shader, 1, &sourceCode, NULL);
+    //Compiles shader
     glCompileShader(shader);
 
+    //Test if compiled correctly
     int success;
     glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
     if (!success){
@@ -76,23 +88,28 @@ unsigned int createShader(GLenum shaderType, const char* sourceCode){
 }
 
 unsigned int createShaderProgram(const char* vertexShaderSource, const char* fragmentShaderSource){
+
+    //Creates vertex and fragment shaders
     unsigned int vertexShader = createShader(GL_VERTEX_SHADER, vertexShaderSource);
     unsigned int fragmentShader = createShader(GL_FRAGMENT_SHADER, fragmentShaderSource);
 
+    //Creates shader program from vertex and fragment shader
     unsigned int shaderProgram = glCreateProgram();
     glAttachShader(shaderProgram, vertexShader);
     glAttachShader(shaderProgram, fragmentShader);
     glLinkProgram(shaderProgram);
 
+    //Test if program linked properly
     int success;
     glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
     if (!success){
         char infoLog[512];
-        glGetProgramInfoLog(shaderProgram,512,NULL,infoLog);
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
         printf("Failed to link shader program: %s", infoLog);
         return 0;
     }
 
+    //Deletes old shaders
     glDeleteShader(vertexShader);
     glDeleteShader(fragmentShader);
 
@@ -118,28 +135,43 @@ int main() {
 		return 1;
 	}
 
+    //Creates shader program
     unsigned int shaderProgram = createShaderProgram(vertexShaderSource, fragmentShaderSource);
 
-    unsigned int vao = createVAO(verticies, 3);
+    //Creates vertex array buffer
+    unsigned int vao = createVAO(verticies, 3, 21);
 
+    //Render loop
 	while (!glfwWindowShouldClose(window)) {
-        float time = (float)glfwGetTime();
+
+        //Gets time
+        float time = static_cast<float>(glfwGetTime());
+
+        //Finds uniform time in shader source code
         int timeLocation = glGetUniformLocation(shaderProgram, "_Time");
+
+        //Sets uniform to time
         glUniform1f(timeLocation, time);
 
+        //Defines the rotation matrix to rotate vertices
         float rotMatrix[9] = {
-                cos(time), 0.0, -sin(time),
-                0.0, 1, 0.0,
-                sin(time), 0.0, cos(time),
+                cos(time), -sin(time), 0.0,
+                sin(time), cos(time), 0.0,
+                0.0, 0.0, 1.0,
                 };
 
+        //Gets matrix uniform location in source code
         int matrixLocation = glGetUniformLocation(shaderProgram, "_RotMatrix");
+        //Sets matrix to rotMatrix
         glUniformMatrix3fv(matrixLocation, 1, GL_FALSE, rotMatrix);
 
 		glfwPollEvents();
-		glClearColor(0.5f, 0.0f, 0.9f, 1.0f);
+
+        //Clear screen
+		glClearColor(abs(sin(time)), abs(sin(time / 3)), abs(sin(time / 2)), 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
+        //Display vertices with shader program, vao, and a draw call
         glUseProgram(shaderProgram);
         glBindVertexArray(vao);
         glDrawArrays(GL_TRIANGLES,0,3);
