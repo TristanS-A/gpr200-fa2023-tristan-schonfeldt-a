7f5547d39c0a3c7728275687786807a6d91a9d01
Tristan Schonfeldt-Aultman <tristan.schonfeldt-a@mymail.champlain.edu>
2023-09-15 11:21:32 -0400

    Added source to shader files

--- assignments/assignment2_sunset/main.cpp
+++ assignments/assignment2_sunset/main.cpp
@@ -25,24 +25,6 @@ float vertices[9] = {
 	 0.0,  0.5, 0.0 
 };
 
-const char* vertexShaderSource = R"(
-	#version 450
-	layout(location = 0) in vec3 vPos;
-	void main(){
-		gl_Position = vec4(vPos,1.0);
-	}
-)";
-
-const char* fragmentShaderSource = R"(
-	#version 450
-	out vec4 FragColor;
-	uniform vec3 _Color;
-	uniform float _Brightness;
-	void main(){
-		FragColor = vec4(_Color * _Brightness,1.0);
-	}
-)";
-
 float triangleColor[3] = { 1.0f, 0.5f, 0.0f };
 float triangleBrightness = 1.0f;
 bool showImGUIDemoWindow = true;
@@ -73,7 +55,7 @@ int main() {
 	ImGui_ImplGlfw_InitForOpenGL(window, true);
 	ImGui_ImplOpenGL3_Init();
 
-	unsigned int shader = createShaderProgram(vertexShaderSource, fragmentShaderSource);
+	unsigned int shader = createShaderProgram(tsa::loadShaderSourceFromFile("vertexShader.vert"), tsa::loadShaderSourceFromFile("fragmentShader.frag"));
 	unsigned int vao = createVAO(vertices, 3);
 
 	glUseProgram(shader);
--- core/tsa/shader.cpp
+++ core/tsa/shader.cpp
@@ -3,7 +3,15 @@
 namespace tsa {
 	std::string tsa::loadShaderSourceFromFile(const std::string& filePath)
 	{
-		return std::string();
+		std::ifstream fstream(filePath);
+		if (!fstream.is_open()) {
+			printf("Failed to load file %s", filePath);
+			return {};
+		}
+		std::stringstream buffer;
+		buffer << fstream.rdbuf();
+		return buffer.str();
+
 	}
 }
 
